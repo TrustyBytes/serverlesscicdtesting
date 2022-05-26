d9e33d19873c383e6e209974c177a00bf448dd9e
Sharan Balakrishnan <78404055+Shamariyan@users.noreply.github.com>
2022-05-26 13:14:49 +0530

    second commit

--- handler.js
+++ handler.js
@@ -1,23 +1,23 @@
-const serverless = require("serverless-http");
-const express = require("express");
+const serverless = require('serverless-http');
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
+app.get('/', (req, res, next) => {
+	return res.status(200).json({
+		message: 'Hello from root!',
+	});
 });
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
+app.get('/hello', (req, res, next) => {
+	return res.status(200).json({
+		message: 'Hello from Sharan',
+	});
 });
 
 app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
+	return res.status(404).json({
+		error: 'get out',
+	});
 });
 
 module.exports.handler = serverless(app);
